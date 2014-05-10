0e1a2d716fdc666bb801b22afb8ed91745ed83a5
jhaip <jhaip@mit.edu>
2014-05-10 15:05:41 -0400

    slowly merging in CGAL stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/linear_least_squares_fitting_3.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/mesh_segmentation.h>
+#include <CGAL/property_map.h>
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -15,7 +23,27 @@
 #include "extra.h"
 #include "camera.h"
 
+#include <stdio.h>
+
 using namespace std;
+
+// Mesh segmentation typedefs:
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point_3;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef Polyhedron::Facet_iterator Facet_iterator;
+typedef Polyhedron::Vertex_iterator Vertex_iterator;
+typedef Polyhedron::Edge_iterator Edge_iterator;
+typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
+typedef Polyhedron::Halfedge_around_vertex_circulator Vertex_halfedge_circulator;
+
+// PCA typedefs:
+typedef double                      FT;
+typedef CGAL::Simple_cartesian<FT>  K;
+typedef K::Line_3                   Line;
+typedef K::Plane_3                  Plane;
+typedef K::Point_3                  Point;
+typedef K::Triangle_3               Triangle;
 
 namespace
 {
@@ -116,6 +144,30 @@ namespace
 
 
 	}
+
+    void parseOFFandPolyhedron(string source) {
+        printf ("%s \n", "A string");
+        /*
+        // create and read Polyhedron
+        Polyhedron mesh;
+        ifstream input("data/cactus.off");
+        if ( !input || !(input >> mesh) || mesh.empty() ) {
+            cerr << "Not a valid off file." << std::endl;
+            //return EXIT_FAILURE;
+        }
+        // create a property-map for segment-ids
+        typedef std::map<Polyhedron::Facet_const_handle, std::size_t> Facet_int_map;
+        Facet_int_map internal_segment_map;
+        boost::associative_property_map<Facet_int_map> segment_property_map(internal_segment_map);
+        // calculate SDF values and segment the mesh using default parameters.
+        size_t number_of_segments = CGAL::segmentation_via_sdf_values(mesh, segment_property_map);
+        cout << "Number of segments: " << number_of_segments << std::endl;
+
+        for(Facet_iterator facet_it = mesh.facets_begin(); facet_it != mesh.facets_end(); ++facet_it) {
+            cout << segment_property_map[facet_it] << " ";
+        }
+        */
+    }
 
 	void parseOFF(string source, string dest){
         string l;
@@ -358,8 +410,8 @@ namespace
         glShadeModel(GL_SMOOTH);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
+    	glEnable(GL_CULL_FACE);
+    	glCullFace(GL_BACK);
 
         // Clear to black
         glClearColor(0,0,0,1);
@@ -434,10 +486,16 @@ int main (int argc, char* argv[]) {
 		cout << "No file inputed. Program terminating." << endl;
 		return 0;
 	}
+    //AllocConsole();
+    //freopen("CONOUT$", "w", stdout); 
+    printf ("Hello world!\n");
+
 	string filef = argv[1];
 	string ext = filef.substr(filef.find_last_of(".")+1);
 	string filename = filef.substr(filef.find_last_of("/")+1);
 	filename = "meshes/"+filename.substr(0, filename.length()-4)+"_model.off";
+
+    cout << "Testing" << endl;
 
 	for (int i = 0; i < ext.length(); i++){
 		ext[i] = tolower(ext[i]);
@@ -448,6 +506,7 @@ int main (int argc, char* argv[]) {
 	}
 	else if (ext == "off"){
 		parseOFF(filef, filename);
+        parseOFFandPolyhedron(filef);
 	}
 
 
